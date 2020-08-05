d7ba8666aa01ac995e53c2399e092d20a58858e1
Mitko Nikov <mitkonikov01@gmail.com>
2020-08-05 23:11:26 +0200

    Firebase initial deploy

--- cinefolio/src/App.svelte
+++ cinefolio/src/App.svelte
@@ -1,8 +1,70 @@
 <script>
+	import MainSlides from './components/MainSlides.svelte';
+	import FooterSnap from './components/FooterSnap.svelte';
+
 	import './theme/common.scss';
 	import './theme/_smui-theme.scss';
 </script>
 
 <main>
-	<h1>Hello App</h1>
+	<div class="bg-img" />
+	<div id="name-tag" class="noselect">
+		<h1>Mitko Nikov</h1>
+	</div>
+
+	<div id="homepage">
+		<MainSlides />
+
+		<div id="footer-container">
+			<div id="footer-flex" class="center-vh">
+				<FooterSnap title="PROJECTS" number="100+" content="Take a look..."></FooterSnap>
+				<FooterSnap title="VIDEOS" number="100+" content="Let's see them!"></FooterSnap>
+				<FooterSnap title="SATISFIED CLIENTS" number="100+" content="Meet my clients."></FooterSnap>
+			</div>
+		</div>
+	</div>
 </main>
+
+<style>
+	.bg-img {
+		overflow: hidden;
+		position: absolute;
+		width: 100vw;
+		height: 100vh;
+		background-image: url('./img/02.jpg');
+		background-repeat: no-repeat;
+		background-size: cover;
+		opacity: 0.3;
+	}
+
+	#homepage {
+		position: relative;
+		width: 100vw;
+		height: 100vh;
+	}
+
+	#name-tag {
+		position: absolute;
+		right: 3em;
+		top: 0.5em;
+		font-size: 18px;
+	}
+
+	#footer-container {
+		position: relative;
+		height: 12em;
+		width: 100%;
+		background-color: rgba(0, 0, 0, 0.3);
+	}
+
+	#footer-flex {
+		display: flex;
+		justify-content: center;
+	}
+	
+    @media screen and (max-width: 39em) {
+		#footer-container {
+			display: none;
+		}
+	}
+</style>
---
+++ cinefolio/src/components/FooterSnap.svelte
@@ -0,0 +1,48 @@
+<script>
+    export let title = '';
+    export let number = '';
+    export let content = '';
+</script>
+
+<div class="footer-snap noselect">
+    <div class="snap-title">{title}</div>
+    <div class="snap-number center-vh">{number}</div>
+    <div class="snap-content">{content}</div>
+    <slot></slot>
+</div>
+
+<style>
+    .footer-snap {
+        position: relative;
+		height: 10em;
+		width: 12em;
+		margin: 0em 1em;
+    }
+    
+    .footer-snap:hover {
+		background-color: rgba(255, 255, 255, 0.05);
+    }
+
+    .snap-title {
+        padding: 0.5em 0.3em 0.3em 0.3em;
+        text-align: center;
+        font-size: 1.1em;
+        font-weight: 300;
+    }
+
+    .snap-number {
+        position: absolute;
+        text-align: center;
+        font-size: 2.5em;
+        font-weight: 700;
+        color: rgb(255, 196, 0);
+    }
+
+    .snap-content {
+        position: absolute;
+        bottom: 0.5em;
+        width: 100%;
+        text-align: center;
+        font-weight: 300;
+    }
+</style>
