dc3b3459d8101703f1ec02b5ebc639dc7b3dda47
Mitko Nikov <mitko.nikov@student.um.si>
2024-02-08 02:00:30 +0100

    Use newer version of docker compose.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-echo " > Running docker-compose down..."
-docker-compose down
-echo " > docker-compose down finished."
+echo " > Running docker compose down..."
+docker compose down
+echo " > docker compose down finished."
 
 echo " > Copy .env"
 cp ~/env/backend.env ~/cine/backend/.env
@@ -10,5 +10,5 @@ echo " > Building Svelte..."
 cd cinefolio2 && bash build.sh
 
 # Start the Docker containers
-echo " > Running docker-compose up..."
-docker-compose up --build -d
+echo " > Running docker compose up..."
+docker compose up --build -d
