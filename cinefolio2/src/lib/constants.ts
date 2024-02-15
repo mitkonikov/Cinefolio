cf782144061ba4a23c63bceb30e40e0208211d51
Mitko Nikov <mitko.nikov@student.um.si>
2024-02-16 00:50:09 +0100

    Update backend URL.

--- cinefolio2/src/lib/constants.ts
+++ cinefolio2/src/lib/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://85.31.239.84/cinefolio/api';
-export const STATIC_URL = 'http://85.31.239.84/cinefolio/public';
+export const API_URL = 'https://vektor.mk/cinefolio/api';
+export const STATIC_URL = 'https://vektor.mk/cinefolio/public';
